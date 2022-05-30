e6790ca5eafab08c4e15ddf50194ee26b12dbc30
0xKubitus <n.cubilier@gmail.com>
2022-05-30 15:57:08 +0200

    fonctionnalité 1 = OK

---
+++ script.js
@@ -0,0 +1,34 @@
+//STEP1 : // Find and store the element we want to listen to events on. 
+var theFooter = document.getElementsByTagName("footer")[0];
+
+//STEP2 : // Define the function that will respond to the event. 
+var onFooterClick = function() {
+  console.log("clique")
+};
+
+//STEP3 : // Add the event listener for the element and function.
+theFooter.addEventListener('click', onFooterClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//STEP1 : // Find and store the element we want to listen to events on. 
+
+
+//STEP2 : // Define the function that will respond to the event. 
+
+//STEP3 : // Add the event listener for the element and function.
+
