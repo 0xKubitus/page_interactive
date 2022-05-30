47cb0f66d493c69a22b37feec0821e67d88889d9
0xKubitus <n.cubilier@gmail.com>
2022-05-30 16:03:12 +0200

    1-bis = OK

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //STEP1 : // Find and store the element we want to listen to events on. 
-var theFooter = document.getElementsByTagName("footer")[0];
+let theFooter = document.getElementsByTagName("footer")[0];
+let i = 1
 
-//STEP2 : // Define the function that will respond to the event. 
+//STEP2 : // Define the function that will respond to the event.  
 var onFooterClick = function() {
-  console.log("clique")
+  console.log("clic n°" + i)
+  i += 1
 };
 
 //STEP3 : // Add the event listener for the element and function.
